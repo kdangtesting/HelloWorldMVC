4d61262f9195f70ee1218df785b84c1e81451b87
Kevin Dang <tmohSuzumiya@gmail.com>
2016-08-04 16:13:43 -0400

    exec bash script trial

--- success.sh
+++ success.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
-cd /home/travis/build/kdangtesting/HelloWorldMVC/target/
-echo $(ls)
+
+if [[ $TRAVIS_EVENT_TYPE == 'push' ]]
+	if [[ $TRAVIS_BRANCH == 'master' ]]
+		echo 'ok to deploy'
+	fi
+else
+	echo 'is it pull?'
+	echo $TRAVIS_PULL_REQUEST
+fi
