402499e8cbc1a4313ad2456684d7a2adc8b498b1
Kevin Dang <tmohSuzumiya@gmail.com>
2016-08-04 17:14:53 -0400

    try again

--- success.sh
+++ success.sh
@@ -8,6 +8,25 @@ if [[ $TRAVIS_EVENT_TYPE == 'push' ]]; then
 		cf --version
 		cf install-plugin -f https://static-ice.ng.bluemix.net/ibm-containers-linux_x64
 		cf plugins
+		cf login -a https://api.ng.bluemix.net -u kdang@ca.ibm.com -p $secretpw
+		cf ic login
+		cf ic images
+		docker build -t tom_cat2 .
+		docker tag tom_cat2 registry.ng.bluemix.net/ahhhh/tom_cat2
+		docker push registry.ng.bluemix.net/ahhhh/tom_cat2
+		cf ic images
+		cf ic ps -a
+		cf ic rename tom_cat2 old_tom_cat2
+		cf ic stop old_tom_cat2
+		cf ic ps -a
+		cf ic rm $(cf ic ps -a -q)
+		cf ic ps -a
+		cf ic run -p 8080 --name tom_cat2 registry.ng.bluemix.net/ahhhh/tom_cat2
+		cf ic ps
+		cf ic ip list
+		cf ic ip unbind 169.44.121.195 old_tom_cat2
+		cf ic inspect tom_cat2
+		cf ic ip bind 169.44.121.195 tom_cat2
 	fi
 else
 	echo 'is it pull?'
